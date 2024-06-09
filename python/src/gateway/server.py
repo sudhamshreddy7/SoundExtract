783006ad79cabb18d886ba6c1afd4ca5da9ca7cd
Sudhamsh Reddy <sudhamshreddy@Sudhamshs-Air.lan>
2024-06-09 17:26:30 -0400

    commit

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -1,2 +1,18 @@
 import os, pika, json, gridfs
 from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongo://host.minikube.internal:27017/videos"
+
+mongo = PyMongo(server)
+fs = gridfs.GridFS(mongo.db)
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+@server.route("/login",methods=["POST"])
+def login():
+    token , err = access.login(request)
+    
